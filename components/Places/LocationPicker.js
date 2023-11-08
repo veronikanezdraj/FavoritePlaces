7317696aa0e87eb935b8c349085d955d04832e05
veronikanezdraj <veronikanezdraj@gmail.com>
2023-11-08 16:49:45 +0200

    Adding a Location Preview Map

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,43 +1,73 @@
-import OutlinedButton from "../UI/OutlinedButton";
-import { StyleSheet, Alert, View } from "react-native";
-import { Colors } from "../../constants/colors";
+import { useState } from "react";
+import { Alert, View, StyleSheet, Image, Text } from "react-native";
 import {
   getCurrentPositionAsync,
   useForegroundPermissions,
   PermissionStatus,
 } from "expo-location";
 
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+import { getMapPreview } from "../../util/location";
+
 function LocationPicker() {
-  const [locationPermissionInfo, requestPermission] =
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions();
 
-  async function verifyPermission() {
-    if (locationPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
       const permissionResponse = await requestPermission();
+
       return permissionResponse.granted;
     }
-    if (locationPermissionInfo.status === PermissionStatus.DENIED) {
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
       Alert.alert(
-        "Insulfficient Permissions!",
+        "Insufficient Permissions!",
         "You need to grant location permissions to use this app."
       );
       return false;
     }
+
     return true;
   }
 
   async function getLocationHandler() {
-    const hasPermission = await verifyPermission();
+    const hasPermission = await verifyPermissions();
+
     if (!hasPermission) {
       return;
     }
+
     const location = await getCurrentPositionAsync();
-    console.log(location);
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
   }
+
   function pickOnMapHandler() {}
+
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton icon="location" onPress={getLocationHandler}>
           Locate User
@@ -61,10 +91,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden",
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    // borderRadius: 4
   },
 });
---
+++ util/location.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyAdD-zvQ2iZ6t-VsyRxl9VhGcekM--mP_g";
+export function getMapPreview(lat, lng) {
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C${lat},-${lng}&key=${GOOGLE_API_KEY}`;
+  return imagePreviewUrl;
+}
