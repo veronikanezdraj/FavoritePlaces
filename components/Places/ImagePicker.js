614f10039fa4415b607bddedee33a92177571149
veronikanezdraj <veronikanezdraj@gmail.com>
2023-11-08 13:19:28 +0200

    Taking Photos on iOS
    managing permissions, add custom button

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,19 +1,73 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Image, Text, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
+
 function ImagePicker() {
+  const [picketImage, setPickedImage] = useState();
+
+  const [useCameraPermissionInfo, requestPermission] = useCameraPermissions();
+  async function verifyPermission() {
+    if (useCameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+    if (useCameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "insulfficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
   async function takeImageHandler() {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image.assets[0].uri);
+    // console.log(image.assets[0].uri);
+  }
+  let imagePreview = <Text>No image taken yet.</Text>;
+  if (picketImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: picketImage }} />;
   }
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take image
+      </OutlinedButton>
     </View>
   );
 }
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
