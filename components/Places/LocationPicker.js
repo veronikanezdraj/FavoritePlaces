6a703c2b21c8db48d67479cc6cd451b5ba689167
veronikanezdraj <veronikanezdraj@gmail.com>
2023-11-09 16:43:41 +0200

    Converting Picked Locations to Human-Readable Addresses

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -13,7 +13,7 @@ import {
 
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 
 function LocationPicker({ onPickLocation }) {
   const [pickedLocation, setPickedLocation] = useState();
@@ -34,8 +34,17 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
-  }, [pickedLocation, onPickLocation]);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation(pickedLocation, { ...onPickLocation, address: address });
+      }
+    }
+    handleLocation();
+  }, [pickedLocation]);
 
   async function verifyPermissions() {
     if (
